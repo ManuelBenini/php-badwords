74d72918afcdee82ee31b1d42a13d744de9fc6c0
ManuelBenini <manuelbenini1905@gmail.com>
2022-06-06 19:11:19 +0200

    Esercizio terminato, stampato in pagina testo iniziale, testo censurato e rimpiazzato con gli asterischi e due metodi alternativi per censusare il testo ma non rimpiazzarlo (e quindi non invalidare il calcolo della lunghezza)

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+
+  $testo = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis laudantium expedita, vitae ipsum assumenda quo sit doloribus iure beatae quasi laboriosam pariatur tempora fugiat earum repellat hic eos similique sint unde ipsa eius ea repellendus. Laboriosam nemo totam voluptas rerum, eius quasi architecto eveniet nesciunt nisi quam aliquam officia excepturi, velit eaque! Modi, totam? Architecto quam recusandae facilis eius reiciendis. Molestiae recusandae quisquam suscipit laudantium tempore amet aliquid magnam quo obcaecati distinctio quibusdam placeat perspiciatis, nesciunt esse dignissimos inventore. Quod itaque reprehenderit cumque id reiciendis repudiandae voluptatum, animi dignissimos deserunt corporis quia, quisquam, voluptatem esse repellendus. Sunt, iure itaque autem nam sapiente adipisci consequatur quam cupiditate dolor veniam rem ex vel modi error inventore omnis quaerat quia earum labore necessitatibus commodi exercitationem quas repellat! Consequuntur earum facilis exercitationem qui, placeat laborum, esse molestiae ea assumenda dolorum, hic doloremque repudiandae maiores optio. Dolor omnis aperiam temporibus impedit iure consequatur accusantium minima quam incidunt unde quo, quia aspernatur, perspiciatis reprehenderit veniam qui exercitationem. Natus dolorem quae sequi officia incidunt placeat molestiae asperiores fuga repellendus magnam, ipsam vel qui iure et modi facere voluptatem non veritatis illum facilis, ipsa fugit earum omnis! Ex fugit dignissimos facilis nostrum obcaecati repellendus eaque. Exercitationem, debitis autem.';
+  
+  
+  $parola_da_censurare = $_GET["censurare"];
+  $testo_censurato = str_replace($parola_da_censurare, '***', $testo);
+
+  //Censurare il testo ma non rimpiazzandolo, metodo 1
+  $testo_censurato_non_rimpiazzato = str_replace($parola_da_censurare, '', $testo);
+
+  //Metodo 2
+  $array_testo_censurato = explode('***', $testo_censurato);
+  $str_testo_censurato = implode('', $array_testo_censurato)
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +26,26 @@
   <title>PHP Badwords</title>
 </head>
 <body>
+
+  <h2>Paragrafo:</h5>
+  <p> <?php echo $testo; ?> </p>
+
+  <h2>Lunghezza paragrafo: <?php echo strlen($testo); ?></h2>
+
+  <h2>Paragrafo censurato:</h5>
+  <p> <?php echo $testo_censurato; ?> </p>
+
+  <h2>Lunghezza paragrafo censurato: <?php echo strlen($testo_censurato); ?></h2>
+
+  <h2>Paragrafo con parola censurata non rimpiazzata:</h5>
+  <p> <?php echo $testo_censurato_non_rimpiazzato; ?> </p>
+
+  <h2>Lunghezza paragrafo censurato: <?php echo strlen($testo_censurato_non_rimpiazzato); ?></h2>
+
+  <h2>Paragrafo con explode ed implode:</h5>
+  <p> <?php echo $str_testo_censurato; ?> </p>
+
+  <h2>Lunghezza paragrafo: <?php echo strlen($str_testo_censurato); ?></h2>
   
 </body>
 </html>
